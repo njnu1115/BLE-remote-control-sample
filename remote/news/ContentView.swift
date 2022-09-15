f5b2cce2c5470be4bd61253394fbb250bd3091f5
Chen Liu <njnu1115@gmail.com>
2022-09-15 13:26:45 +0800

    add a picker, act as gear, reduce operation button to left, speed and right

--- remote/news/ContentView.swift
+++ remote/news/ContentView.swift
@@ -7,51 +7,38 @@ struct ContentView: View {
     @State var presented: Bool = false
     @State var isConnected: Bool = Bluetooth.shared.current != nil { didSet { if isConnected { presented.toggle() } } }
 // isConnected 的判断在这里似乎不工作， 现在还不知道怎么在isConnected的时候下钩子来更新这里的UI控件，TBD
+    @State private var gear = 0
 
     var body: some View {
+        
+
+        
         VStack(alignment: .center) {
-            HStack{
-//                Text("Bluetooth is \(isConnected ? "Available":"Unavailable")")
-//                Image(systemName: isConnected ? "checkmark":"xmark")
-//                    .foregroundColor(isConnected ?  .green:.red)
-            }.font(.largeTitle)
+            Picker("Select Gear", selection: $gear) {
+                Text("D")
+                    .tag(0)
+                Text("R")
+                    .tag(1)
+            }
+            .pickerStyle(.segmented)
             
-            HStack.init(alignment: .center, spacing: 0){
-                
-/// todo: 把这个按钮换成一个像是车上仪表盘显示档位切换D档的那种样式
-            Button(action: {
-                bluetooth.send([0x0044]) //UTF-8 code for "D"
-                      }, label: {
-                          Image("btn_forward")
-                              .resizable()
-                              .aspectRatio(contentMode: .fit)
-                      }).frame(width: 100, height: 100)
-/// todo: 把这个按钮换成一个像是车上仪表盘显示档位切换R档的那种样式
-            Button(action: {
-                bluetooth.send([0x0052]) //UTF-8 code for "R"
-                      }, label: {
-                          Image("btn_backward")
-                              .resizable()
-                              .aspectRatio(contentMode: .fit)
-                      }).frame(width: 100, height: 100)
-            }.font(.largeTitle)
             HStack.init(alignment: .center, spacing: 20) {
                 Button(action: {
-                bluetooth.send([0x004C])  //UTF-8 code for "L"
+                bluetooth.send([0 == gear ? 0x0042 : 0x0043]) //42:LeftForward, 43:LeftReverse
                       }, label: {
                           Image("btn_left")
                               .resizable()
                               .aspectRatio(contentMode: .fit)
                       }).frame(width: 100, height: 100)
                 Button(action: {
-                bluetooth.send([0x0046])  //UTF-8 code for "F"
+                    bluetooth.send([0 == gear ? 0x0044 : 0x0045]) //44: Forward, 45:Reverse
                       }, label: {
-                          Image("btn_forward")
+                          Image(0 == gear ? "btn_forward" : "btn_backward")
                               .resizable()
                               .aspectRatio(contentMode: .fit)
                       }).frame(width: 100, height: 100)
                 Button(action: {
-                bluetooth.send([0x0052])  //UTF-8 code for "R"
+                    bluetooth.send([0 == gear ? 0x0046 : 0x0047]) // 46: RightForward, 47:RightReverse
                       }, label: {
                           Image("btn_right")
                               .resizable()
